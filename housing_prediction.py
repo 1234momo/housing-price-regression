ce9d47d381e844c97b26b254a88943665c57f505
williamtnguyen <williamnguyen214@gmail.com>
2020-04-22 16:19:31 -0700

    initial commit

---
+++ housing_prediction.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+housing_data = pd.read_csv('./csv/housing.csv')
+print(housing_data.head(5))
+print(housing_data.info())
+
+housing_data.hist(bins=50, figsize=(15,15))
+plt.show()
