e7ec5b4e9aae341bf0f423a036697482120d0b80
Gary <garychang9@gmail.com>
2020-04-23 04:02:41 -0700

    fix loop for csv. runtime is too long

--- reverseGeo.js
+++ reverseGeo.js
@@ -5,17 +5,18 @@ let API_KEY = "AIzaSyD-Fwlf4-W04pkqjQly5kcP3VNfRSGWn2w";
 // Convert csv to json
 let json = csvToJson.fieldDelimiter(',').getJsonFromCsv("./csv/housing.csv");
 
-// Test 1 coordinate
-let coordinates = `${json[1].latitude},${json[1].longitude}`;
+for (let i = 0; i < Object.keys(json).length; ++i) {
+  let coordinates = `${json[i].latitude},${json[i].longitude}`;
+  getAddress(coordinates, i+1);
+}
 
-console.log("\'" + coordinates + "\'");
-getAddress(coordinates);
-
-async function getAddress(coordinates) {
-    try {
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates}&key=${API_KEY}`);
-      console.log(response.data.results);
-    } catch (error) {
-      console.error(error);
-    }
+async function getAddress(coordinates, index) {
+  try {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates}&key=${API_KEY}`);
+    console.log(index + ": " + response.data.results[0].formatted_address);
+  } 
+  catch (error) {
+    console.error(index + ": Unable to locate");
+    // console.error(error);
   }
+}
