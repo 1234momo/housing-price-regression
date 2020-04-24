154ea1c0a9a5817e046df073d59b43b81ac0550b
Gary <garychang9@gmail.com>
2020-04-23 21:48:23 -0700

    Finished reverse geo, need to play around with zillow

--- reverseGeo.js
+++ reverseGeo.js
@@ -2,13 +2,17 @@
 const csvToJson = require('convert-csv-to-json'); 
 const axios = require('axios').default;
 let API_KEY = "AIzaSyD-Fwlf4-W04pkqjQly5kcP3VNfRSGWn2w";
+var stringify = require('csv-stringify');
+var fs = require('fs');
+
 
 // Variables for when valid addresses are found
 let foundAddresses = {
   "addresses" : []
 };
+foundAddresses['addresses'].push(["row", "address"]);
 var numAddresses = 0;
-var max = 10;
+var max = 20;
 
 // Convert csv to json
 let json = csvToJson.fieldDelimiter(',').getJsonFromCsv("./csv/housing copy.csv");
@@ -20,23 +24,45 @@ function main () {
 
     getAddress(coordinates, i)
     .then(({result, row}) => {
+
+      // If result is a string, valid address, and the max hasn't been reached, add the address into foundAddresses
       if (result != null && isValidAddress(result) && numAddresses < max) {
         ++numAddresses;
-        row = row.toString();
-        foundAddresses['addresses'].push({row : result});
+        let observation = [];
+        observation.push(row); 
+        observation.push(result);
+        foundAddresses['addresses'].push(observation);
+
         console.log("GOT A HIT: " + numAddresses + " for " + result);
         console.log(foundAddresses.addresses);
       }  
+
+      // Save JSON into CSV
+      if (numAddresses == max) {
+        stringify(foundAddresses.addresses, function(err, output) {
+          fs.writeFile('name.csv', output, 'utf8', function(err) {
+            if (err) {
+              console.log('Some error occured - file either not saved or corrupted file saved.');
+            } else {
+              console.log('It\'s saved!');
+            }
+          });
+        });
+
+        // Increment numAddresses so that this "if" block doesn't run again for other processes
+        ++numAddresses;
+      }
     })
     .catch((error) => {});
   }
 }
 
+// Get address associated with longitude and latitude coordinates
 async function getAddress(coordinates, index) {
   try {
     const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates}&key=${API_KEY}`);
     let address = response.data.results[0].formatted_address;
-    return {result: address, row: index};
+    return {result: address, row: index.toString()};
   } 
   catch (error) {
     return;
@@ -55,4 +81,5 @@ function isValidAddress(address) {
   return false;
 }
 
-main()
+// Start of program
+main();
